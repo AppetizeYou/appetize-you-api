7c7b8817d35fa88b903e2b9f1352be66fe5632a1
Pogeun <jiawy1189@gmail.com>
2022-07-16 12:02:15 +1000

    added User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "/auth" do
+    post "/sign_up", to: "users#create"
+    post "/sign_in", to: "users#sign_in"
+    put "/", to: "users#update"
+    patch "/", to: "users#update"
+    delete "/", to: "users#destroy"
+  end
 end
